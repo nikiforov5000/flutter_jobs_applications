f14f13d0a7e419cba8a56fc02254e7e6f8ddc3db
boris <nikiforov5000@gmail.com>
2023-04-25 16:36:29 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: MainScreen(),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InputSection(),
+        // JobsStreamBuilder(),
+      ],
+    );
+  }
+}
+
+class InputSection extends StatefulWidget {
+  const InputSection({Key? key}) : super(key: key);
+
+  @override
+  State<InputSection> createState() => _InputSectionState();
+}
+
+class _InputSectionState extends State<InputSection> {
+  TextEditingController companyController = TextEditingController();
+  TextEditingController urlController = TextEditingController();
+  TextEditingController countryController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        InputFieldWidget(label: 'Company', controller: companyController),
+        InputFieldWidget(label: 'URL', controller: urlController),
+        InputFieldWidget(label: 'Country', controller: countryController),
+        InputFieldWidget(label: 'Title', controller: titleController),
+      ],
+    );
+  }
+}
+
+class InputFieldWidget extends StatelessWidget {
+  InputFieldWidget({required this.label, required this.controller});
+  final String label;
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final noUnderline = InputDecoration(border: InputBorder.none);
+    final textField = TextField(controller: controller, decoration: noUnderline,);
+
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Center(child: Text(label))),
+          Expanded(child: textField)
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
