393b0d54b1fefb48e1e721fba9f4d19d30cdc38e
boris <nikiforov5000@gmail.com>
2023-05-12 13:47:53 +0300

    build and deploy

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: spacer),
       child: Column(
         children: [
-          CoverLetterEditorButton(),
+          // CoverLetterEditorButton(),
           InputSection(),
           JobsStreamBuilder(),
         ],
